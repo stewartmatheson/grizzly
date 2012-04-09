1587c3a04614c985de7ebc3d2249f3625a28dd94
Stewart Matheson <stewart.matheson@gmail.com>
2012-04-10 08:46:53 +1000

    Updated VCR settings to fix tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -17,4 +17,8 @@ end
 VCR.configure do |c|
   c.cassette_library_dir = 'spec/cassettes'
   c.hook_into :webmock
+  c.preserve_exact_body_bytes do |http_message|
+    http_message.body.encoding.name == 'ASCII-8BIT' ||
+    !http_message.body.valid_encoding?
+  end
 end
